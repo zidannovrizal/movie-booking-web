4a46d9c5012949c4116d597982cad0f14251e1e5
zidannovrizal <zidannovrizal098@gmail.com>
2025-05-07 00:32:38 +0700

    fix cors

--- src/api/config.ts
+++ src/api/config.ts
@@ -7,6 +7,7 @@ const api = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
+  withCredentials: true, // Enable sending cookies in cross-origin requests
 });
 
 // Add a request interceptor
